4475b7882a01427dc97e23328f2d381c5e289831
champ24 <jagtapsuyog2001@gmail.com>
2020-03-24 19:11:48 +0530

    Login society 0.1

--- app/src/main/java/com/example/login_society/Joining_form.java
+++ app/src/main/java/com/example/login_society/Joining_form.java
@@ -3,8 +3,19 @@ package com.example.login_society;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Joining_form extends AppCompatActivity {
+
+    TextInputEditText fullname,phoneno,email,officeno,societyname,societyaddr,passcode,confopasscode;
+    Button reg_btn;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
